7e89d28a1cdbe2b531ebdbd14bb7567fec0696e2
Jasmitsingh014 <jasmits007@gmail.com>
2024-12-24 19:00:46 +0530

    Update root dir👌👍❤️

--- app.ts
+++ app.ts
@@ -6,6 +6,13 @@ app.use(Express.json());
 
 app.use('/api/user',Proxy('http://localhost:9000'))
 
+app.use('/api/task',Proxy('http://localhost:9001'))
+
+app.use('/api/client',Proxy('http://localhost:9002'))
+
+app.use('/api/field',Proxy('http://localhost:9003'))
+
+app.use('/api/blog',Proxy('http://localhost:9004'))
 
 
 
--- src/blogs/app.ts
+++ src/blogs/app.ts
@@ -1,8 +1,11 @@
 import Exprees from 'express'
 import cookiePaser from 'cookie-parser'
+import router from './routes/Index';
 
 const app=Exprees();
 app.use(Exprees.json());
 app.use(cookiePaser())
+
+app.use('/',router)
 
 export default app;
