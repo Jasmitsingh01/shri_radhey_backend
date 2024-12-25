92260767c26341f27864858a353946b1c05a1e8f
Jasmitsingh014 <jasmits007@gmail.com>
2024-12-25 23:37:08 +0530

    remove utlis from root and update all package.json with buld and start Command

--- src/utlis/response/responseHandler.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from 'express';
-import ResponseData from './responseData';
-function ResponseHandler(res: Response, data?: ResponseData, status: number = 200) {
-    res.status(status).send(data);
-}
-export default ResponseHandler;
