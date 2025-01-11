15c7dd2cb0d0d96c8e161973607b4a1a25d3cdd3
Jasmitsingh014 <jasmits007@gmail.com>
2025-01-11 19:17:23 +0530

    Fix pipeline bugs

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config()
 import app from "./app";
 
-const port = process.env.PORT|| 3000;
+const port = process.env.PORT|| 5000;
 
 app.listen(port,()=>{
     console.log(`Main Server Runnig on ${port} with Url : http://localhost:${port}`)
---
+++ strat.sh
@@ -0,0 +1,51 @@
+sudo npm install 
+
+cd src/user
+
+sudo npm install
+
+sudo npm run build
+
+cd ../..
+
+cd src/task
+
+sudo npm install
+
+sudo npm run build
+
+cd ../..
+
+cd src/fields
+
+sudo npm install
+
+sudo npm run build
+
+
+cd ../..
+
+
+
+cd src/client
+
+sudo npm install
+
+sudo npm run build
+
+cd ../..
+
+
+
+cd src/blogs
+
+sudo npm install
+
+sudo npm run build
+
+cd ../..
+
+
+sudo npm run build
+
+sudo npm run start
