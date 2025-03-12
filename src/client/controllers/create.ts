df5d7bbfa0d2da345dad3eaae2d7b19dcf13ba42
Jasmitsingh014 <jasmits007@gmail.com>
2025-03-13 00:55:52 +0530

    fix bug

--- src/client/controllers/create.ts
+++ src/client/controllers/create.ts
@@ -3,7 +3,7 @@ import RequestHandler from "../utlis/request/requestHandler";
 import ResponseData from "../utlis/response/responseData";
 import ResponseHandler from "../utlis/response/responseHandler";
 import client from "../models/client.model";
-import { Request,Response ,NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express'
 import UploadImageOnline from "../utlis/cloudnairy";
 
 
@@ -12,11 +12,11 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
     try {
 
 
-     
-        const { fullname, contact, height, gender, birth, ethinicity, email , fulladdreess, qualification, occupation, member, disablitiy, blood_group, marital_status, body_type, complexion, use_specatils, family, meal, abroad, belive_in_patri, open_for_other_caste, income,astrology ,native,sibling,perferance} = req.body;
 
-         const file= req.file;
-        
+        const { fullname, contact, height, gender, birth, ethinicity, email, fulladdreess, qualification, occupation, member, disablitiy, blood_group, marital_status, body_type, complexion, use_specatils, family, meal, abroad, belive_in_patri, open_for_other_caste, income, astrology, native, sibling, perferance } = req.body;
+
+        const file = req.file;
+
         const { firstname, lastname } = fullname;
         const { phone, whatsaap_number } = contact;
         const { value, unit } = height;
@@ -26,15 +26,73 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
         const { qualification: qualification_name, details: qualification_details } = qualification;
         const { occupation: occupation_name, details: occupation_details } = occupation;
         const { use, power } = use_specatils;
-        const { father: { name: father_name, occupation: father_occupation ,occupation_Details:father_occupation_detalis }, mother: { name: mother_name, occupation: mother_occupation ,occupation_Details:mother_occupation_detalis}, details, number_of_member ,house_status} = family
+        const { father: { name: father_name, occupation: father_occupation, occupation_Details: father_occupation_detalis }, mother: { name: mother_name, occupation: mother_occupation, occupation_Details: mother_occupation_detalis }, details, number_of_member, house_status } = family
         const { stauts, expries, package: { name: package_name, amount_paid }, budget, source } = member;
         const { diet, smoking, drinking } = meal;
         const { is_willing, mention_country } = abroad;
         const { family: family_income, personal: personal_income } = income;
 
 
+        const ExitsClient = await client.find({
+            $and: [{
+                "fullname.firstname": firstname,
+                "fullname.lastname": lastname,
+                "contact.phone": phone,
+                "contact.whatsaap_number": whatsaap_number,
+                "contact.email": email,
+                "height.value": value,
+                "height.unit": unit,
+                gender: gender,
+                "birth.date": date,
+                "birth.place": place,
+                "birth.time": time,
+                preferences: perferance, // Corrected spelling
+                "ethinicity.religion": religion,
+                "ethinicity.caste": caste,
+                "ethinicity.gotra": gotra,
+                "fulladdreess.country": country,
+                "fulladdreess.state": state,
+                "fulladdreess.city": city,
+                "fulladdreess.pincode": pincode,
+                "fulladdreess.custom": custom ? custom : `${city || ''}${state || ''}${country || ''}-${pincode || ''}`,
+                astrology: astrology, // Corrected spelling
+                "qualification.qualification": qualification_name,
+                "qualification.details": qualification_details,
+                "occupation.occupation": occupation_name,
+                "occupation.details": occupation_details,
+                "member.stauts": stauts,
+                "member.expires_member": expries, // Corrected spelling
+                "member.package.name": package_name,
+                "member.package.amount_paid": amount_paid,
+                "member.budget": budget,
+                "member.source": source,
+                disability: disablitiy, // Corrected spelling
+                blood_group: blood_group,
+                marital_status: marital_status,
+                complexion: complexion,
+                body_type: body_type,
+                "family.father.name": father_name,
+                "family.father.occupation": father_occupation,
+                "family.father.occupation_Details": father_occupation_detalis, // Corrected spelling
+                "family.mother.name": mother_name,
+                "family.mother.occupation": mother_occupation,
+                "family.mother.occupation_Details": mother_occupation_detalis, // Corrected spelling
+                "family.details": details,
+                "family.number_of_member": number_of_member,
+                "family.house_status": house_status,
+                native: native,
+                siblings_details: sibling,
+                "meal.diet": diet,
+                "meal.smoking": smoking,
+                "meal.drinking": drinking,
+                "income.family": family_income,
+                "income.personal": personal_income,
+            }]
+        });
 
-
+        if (ExitsClient) {
+            throw new error('Client Already Extis', 400)
+        }
 
 
         const NewClient = new client({
@@ -57,7 +115,7 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
                 place,
                 time
             },
-            perfrences:perferance,
+            perfrences: perferance,
             ethinicity: {
                 religion,
                 caste,
@@ -70,7 +128,7 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
                 custom: custom ? custom : city + state + country + '-' + pincode,
                 pincode
             },
-            astroligy:astrology,
+            astroligy: astrology,
             qualification: {
                 qualification: qualification_name,
                 details: qualification_details,
@@ -96,37 +154,37 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
             complexion,
             body_type,
             use_specatils: {
-                use:use!=='YES'? false :true,
+                use: use !== 'YES' ? false : true,
                 power
             },
             family: {
                 father: {
                     name: father_name,
                     occupation: father_occupation,
-                    occupation_Details:father_occupation_detalis
+                    occupation_Details: father_occupation_detalis
                 },
                 mother: {
                     name: mother_name,
                     occupation: mother_occupation,
-                    occupation_Details:mother_occupation_detalis
+                    occupation_Details: mother_occupation_detalis
                 },
                 details,
                 number_of_member,
-                house_status:house_status
+                house_status: house_status
             },
-            native:native,
-            siblings_details:sibling,
+            native: native,
+            siblings_details: sibling,
             meal: {
                 diet,
                 smoking,
                 drinking,
             },
             abroad: {
-                is_willing:is_willing!=='YES'? false :true,
+                is_willing: is_willing !== 'YES' ? false : true,
                 mention_country
             },
-            belive_in_patri:belive_in_patri!=='YES'? false :true,
-            open_for_other_caste:open_for_other_caste!=='YES'? false :true,
+            belive_in_patri: belive_in_patri !== 'YES' ? false : true,
+            open_for_other_caste: open_for_other_caste !== 'YES' ? false : true,
             income: {
                 family: family_income,
                 personal: personal_income
@@ -134,11 +192,11 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
 
 
         })
-        if ( file) {
-            const imageurl= await UploadImageOnline((file as any )?.path || '');
-             if(!imageurl){
-                throw new error('failed to upload image',500)
-             }
+        if (file) {
+            const imageurl = await UploadImageOnline((file as any)?.path || '');
+            if (!imageurl) {
+                throw new error('failed to upload image', 500)
+            }
             NewClient.profile_image = imageurl;
 
         }
@@ -154,9 +212,9 @@ const CreateClient = RequestHandler(async (req: Request, res: Response, next: Ne
         ResponseHandler(res, response, 201)
     } catch (error) {
         console.error(error)
-        const response= new ResponseData(error,(error as any).statusCode || (error as any).status || 500,(error as any).message);
+        const response = new ResponseData(error, (error as any).statusCode || (error as any).status || 500, (error as any).message);
 
-    ResponseHandler(res,response,(error as any).statusCode || (error as any).status || 500)
+        ResponseHandler(res, response, (error as any).statusCode || (error as any).status || 500)
 
     }
 
